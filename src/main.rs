c32beb2b83dec2879e4c4a3605868243da7b24ac
Hiroyuki Sato <hiroysato@gmail.com>
2022-07-17 00:52:06 +0900

    Add struct

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,49 @@ use std::io::BufReader;
 use std::io::prelude::*;
 use regex::Regex;
 
+struct Pci {
+    code: u16,
+    name: String,
+}
+
+struct PciSub {
+    code: u16,
+    name: String,
+    dev_code: u8,
+    dev_name: String,    
+}
+
+struct PciSubDev {
+    code: u16,
+    name: String,
+    dev_code: u8,
+    dev_name: String,
+    sub_vend: u8,
+    sub_code: u8,
+    sub_name: String,
+}
+
+
+fn hex_to_num(str: &String) -> u16 {
+
+    let v = u16::from_str_radix(str,16);
+    return v.unwrap();
+}
+
 fn main() {
 
     let f = File::open("/usr/share/misc/pci.ids").unwrap();
     let reader = BufReader::new(f);
     let re_comment = Regex::new("^\\s*(#|$)").unwrap();
-    let re_pci_dev = Regex::new("^[0-9a-f]{4}\\s+(.*)").unwrap();
+    let re_pci_dev = Regex::new("^([0-9a-f]{4})\\s+(.*)").unwrap();
     let re_pci_sub = Regex::new("^\\t[0-9a-f]{4}\\s+(.*)").unwrap();
     let re_pci_sdv = Regex::new("^\\t\\t[0-9a-f]{4}\\s+[0-9a-f]{4}\\s+(.*)").unwrap();
 
     let re_dev_cls = Regex::new("^C\\s+[0-9a-f]{2}\\s+(.*)").unwrap();
     let re_dev_sub = Regex::new("\\t[0-9a-f]{2}\\s+(.*)").unwrap();
     let re_dev_prg = Regex::new("\\t\\t[0-9a-f]{2}\\s+(.*)").unwrap();
+
+    println!("{}",hex_to_num(&"FF".to_string()));
 
     for line in reader.lines() {
         let l = line.unwrap();
@@ -24,8 +55,11 @@ fn main() {
             None => (),
         };
         match re_pci_dev.find(&l) {
-//            Some(_) => println!("{}",l),
-            Some(_) => continue,
+            Some(_) => {
+                let cap = re_pci_dev.captures(&l).unwrap();
+                println!("code: {}, name: {:?}",cap.get(1).map_or("",|m| m.as_str() ), cap.get(2).map_or("",|m| m.as_str() ));
+            },
+//            Some(_) => continue,
             _ => (),
         }
         match re_pci_sdv.find(&l) {
@@ -55,6 +89,6 @@ fn main() {
             _ => (),
         }
 
-        println!("{}",l);
+//        println!("{}",l);
     } 
 }
